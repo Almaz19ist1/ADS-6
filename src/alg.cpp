cc278057cc6d505695b94e9fb1c79a24ac752a21
Almazotinus <62652018+Almaz19ist1@users.noreply.github.com>
2020-05-31 11:43:38 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,27 @@
 
 BST<std::string> makeTree(char* filename)
 {
-
+  ifstream f;
+  f.open(filename);
+  std::string str;
+  BST<std::string> bst;
+  std::string str1 = '';
+  
+  while(getline(f, str))
+  {
+    for(int i = 0; i <strlen(str); i++)
+    {
+      if(str[i + 1] != 32)
+      {
+        str1 += str[i]; 
+      }
+      else
+      {
+        bst.add(str1); 
+      }
+    }
+    str1 = '';
+  }
+  
+  f.close(filename);
 }
