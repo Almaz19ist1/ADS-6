eda70582e22034f2c9370c4c262b53ebb0fb0325
Almazotinus <62652018+Almaz19ist1@users.noreply.github.com>
2020-05-31 11:25:30 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,1 +1,58 @@
 #include <iostream>
+
+
+
+template<typename T>
+class BST
+{
+public:
+    struct Node
+    {
+        T value;
+        int count;
+        Node *left;
+        Node *right;
+    };
+private:
+    Node* root;
+    Node* addNode(Node *, T);
+public:
+    BST();
+    ~BST();
+    void add(T);
+};
+
+template<typename T>
+BST<T>::BST():root(nullptr) {}
+
+template<typename T>
+BST<T>::~BST() 
+{
+    if(root)
+        delTree(root);
+}
+
+
+template<typename T>
+typename BST<T>::Node* BST<T>::addNode(Node *root, T value) {
+   if(root==nullptr) {
+      root=new Node;
+      root->value=value;
+      root->count=1;
+      root->left=root->right=nullptr;
+   }
+   else if(root->value>value) {
+     root->left=addNode(root->left,value);
+   }
+   else if(root->value<value) {
+     root->right=addNode(root->right,value);
+   }
+   else
+    root->count++;
+   return root;
+}
+
+template<typename T>
+void BST<T>::add(T value) {
+   root=addNode(root,value);
+}
